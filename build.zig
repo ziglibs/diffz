f1aca79fb9dd17877f3f0baec99ea9a32392bb13
Yuri Pieters <yuri@zopatista.com>
2023-03-05 17:57:15 +0000

    Use new addModule syntax

--- build.zig
+++ build.zig
@@ -1,8 +1,7 @@
 const std = @import("std");
 
 pub fn build(b: *std.Build) void {
-    b.addModule(.{
-        .name = "diffz",
+    _ = b.addModule("diffz", .{
         .source_file = .{ .path = "DiffMatchPatch.zig" },
     });
 }
