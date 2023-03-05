a45b2d4c532a9550460fb75b5558c26a445a41fc
Travis Staloch <twostepted@gmail.com>
2023-03-05 10:54:54 -0800

    use new addModule api

--- build.zig
+++ build.zig
@@ -1,8 +1,5 @@
 const std = @import("std");
 
 pub fn build(b: *std.Build) void {
-    b.addModule(.{
-        .name = "diffz",
-        .source_file = .{ .path = "DiffMatchPatch.zig" },
-    });
+    _ = b.addModule("diffz", .{ .source_file = .{ .path = "DiffMatchPatch.zig" } });
 }
