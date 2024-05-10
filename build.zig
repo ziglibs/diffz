ef45c00d655e5e40faf35afbbde81a1fa5ed7ffb
John <117967760+mochalins@users.noreply.github.com>
2024-05-10 11:35:08 +0900

    scm, fix: Update build to use `b.path` (#20)

--- build.zig
+++ build.zig
@@ -2,6 +2,6 @@ const std = @import("std");
 
 pub fn build(b: *std.Build) void {
     _ = b.addModule("diffz", .{
-        .root_source_file = .{ .path = "DiffMatchPatch.zig" },
+        .root_source_file = b.path("DiffMatchPatch.zig"),
     });
 }
