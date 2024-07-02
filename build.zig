9c27090a3baa7ca38b0a272552a7a7708a677f95
Sam Atman <atmanistan@gmail.com>
2024-07-01 20:24:48 -0400

    Add build.zig.zon, update build.zig, .gitignore
    This adds a test running step and a zig.zon manifest.  Also adds to the
    .gitignore the new location of caches, .zig-out.

--- build.zig
+++ build.zig
@@ -1,7 +1,33 @@
 const std = @import("std");
 
 pub fn build(b: *std.Build) void {
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+
     _ = b.addModule("diffz", .{
         .root_source_file = b.path("DiffMatchPatch.zig"),
     });
+
+    const lib = b.addStaticLibrary(.{
+        .name = "diffz",
+        .root_source_file = b.path("DiffMatchPatch.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+
+    // This declares intent for the library to be installed into the standard
+    // location when the user invokes the "install" step (the default step when
+    // running `zig build`).
+    b.installArtifact(lib);
+
+    // Run tests
+    const tests = b.addTest(.{
+        .name = "tests",
+        .root_source_file = b.path("DiffMatchPatch.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+    const step_tests = b.addRunArtifact(tests);
+
+    b.step("test", "Run diffz tests").dependOn(&step_tests.step);
 }
